45e404a2eb54c30303fb7d799aac7a6246ed80ca
Erik <65229112+EjayGit@users.noreply.github.com>
2025-02-26 08:17:00 +0000

    Update StartGameScreen.js

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {TextInput, View} from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
+function StartGameScreen() {
+    return (
+    <View style={styles.inputContainer}>
+        <TextInput style={styles.numberInput} maxLength={2}/>
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
     </View>
-  );
+    );
 }
 
+export default StartGameScreen;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    inputContainer: {
+        flex: 1,
+        padding: 16,
+        marginTop: 100,
+        backgroundColor: '#27063c',
+        marginHorizontal: 24,
+        borderRadius: 8,
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOffset: {width:0 , height:2},
+        shadowRadius: 6,
+        shadowOpacity: 0.25,
+    },
+    numberInput:{
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
 });
