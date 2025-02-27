90c5b9fe39b20f26e3206a3265fd9a5b13f56b83
Erik <65229112+EjayGit@users.noreply.github.com>
2025-02-27 09:56:04 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,32 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StartGameScreen from './screens/StartGameScreen';
 
-export default function App() {
-  return (import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StartGameScreen/>
   );
 }
 
